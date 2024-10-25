d6d8c59a6a5a8d915d0ba3481298490c4cc04983
John Doe <johndoe@example.com>
2024-10-25 16:24:34 +0200

    Doens't work 28

---
+++ basics-03-events-starting-code/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+      confirmedName: "", // aftre enter on fomr
+    };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    remove(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    submitForm(event) {
+      // event.preventDefault();
+      alert("submitted");
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+  },
+});
+
+app.mount("#events");
