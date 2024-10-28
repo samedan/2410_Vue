88b8fc4d12740a237436168469bf069a74f8ea4c
John Doe <johndoe@example.com>
2024-10-28 11:45:47 +0100

    Computed properties & Watchers

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,66 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+      lastName: "",
+      // fullname: "",
+    };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        // setTimeout(fucntion(){}, 2000)
+        this.counter = 0;
+      }
+    },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = this.name + " " + value;
+    //   }
+    // },
+  },
+  computed: {
+    fullname() {
+      console.log("running again");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      // return this.name + " " + "Pop";
+      return this.name + " " + this.lastName;
+    },
+  },
+  methods: {
+    outputFullName() {
+      console.log("running again");
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Pop";
+    },
+    setName(event, lastName) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
+});
+
+app.mount("#events");
